5934a3e9e93fac8b0cc592bc78abe6a6f1e85fc7
jesusrugarcia <jesusrugarciasandia@gmail.com>
2023-03-07 12:15:01 +0000

    verification agent implemented, untested

--- Implementation/VerificationAgent/src/controllers/verify.js
+++ Implementation/VerificationAgent/src/controllers/verify.js
@@ -1,7 +1,25 @@
+const verifyUpdate = require('../services/verify-update');
+const fs = require('fs');
 
-
-const verify = async() => {
-
+const verify = async(req) => {
+    try{
+        var payload;
+        if(req.file){
+            payload = fs.readFileSync(req.file.path,'base64');
+        } else {
+            payload = req.body.payload
+        }
+        var verified = verifyUpdate(req.body.publicKey,req.body.update,payload);
+        return ({
+            status : 200,
+            message : 'Update Verified: ' + verified
+        })
+    } catch (err){
+        return ({
+            status : 500,
+            message : err
+        })
+    }
 }
 
 module.exports = verify;
