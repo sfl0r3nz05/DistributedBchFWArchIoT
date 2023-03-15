5c401629e85b37945d9ba83d650c86df9bf1e252
jesusrugarcia <jesusrugarciasandia@gmail.com>
2023-03-15 12:07:52 +0000

    o

--- Implementation/author-app/src/services/test-register.js
+++ Implementation/author-app/src/services/test-register.js
@@ -82,7 +82,7 @@ const test = async (times,filePath) =>{
     
     for(var i = 0; i < times; i++){
         var jsonit = JSON.parse(stringify(json));
-        jsonit.update.manifest.versionID = 'Test15MB'+i;
+        jsonit.update.manifest.versionID = 'Tese'+i;
         //delete jsonit.payload;
         await signManifestJson(jsonit, privateKey);
         //console.log(jsonit.update)
@@ -94,4 +94,4 @@ var test552 = './BCM2046A2-iMac2009Bluetooth.bin';
 var test95 = './9.5MBTest.bin'
 var test154 = './15.4MBTest.bin'
 var test361 = './36.1MBTest.bin'
-test(100,test154);
+test(1,test552);
